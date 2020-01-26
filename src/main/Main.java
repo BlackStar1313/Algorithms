fb53ebc5907698c343d5f463f707fc74859519da
peguy <pegpeg07@hotmail.com>
2020-01-26 03:57:07 +0100

    testing algorithms

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@ import algorithm.ShuntingYardAlgorithm;
 import enums.OperatorType;
 import helper.GenericHelper;
 import multithreading.ParallelMergeSortTester;
+import multithreading.matrixmultiplication.Matrix;
+import multithreading.matrixmultiplication.ParallelMatrixMultiplication;
 import sorting.Quick;
 import sorting.Selection;
 import sorting.Shell;
@@ -13,6 +15,16 @@ import structures.*;
 public class Main{
 
 	public static void main(String[] args){
-		new Calculator(new RpnEvaluationAlgorithm(), null);
+
+		//new Calculator(new RpnEvaluationAlgorithm(), null);
+
+		Matrix m1 = new Matrix(4000, 4000);
+		Matrix m2 = new Matrix(4000, 4000);
+
+		m1.fill(10, 20);
+		m2.fill(10, 20);
+
+		ParallelMatrixMultiplication pl = new ParallelMatrixMultiplication(m1, m2);
+		pl.startProcessing();
 	}
 }
